c0f095ca3209cb8b2e415e0c2cb885c6c8f1aa19
Rodrigo Montoya <montoyar32@gmail.com>
2023-03-03 20:57:42 -0500

    Fixed bugs. Added edit modal.

--- tukan-brain-teaser/src/App.jsx
+++ tukan-brain-teaser/src/App.jsx
@@ -28,7 +28,6 @@ ChartJS.register(
 );
 
 function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
       columns,
@@ -74,10 +73,27 @@ function Table({ columns, data }) {
   );
 }
 
+function round(num, places) {
+  return +(Math.round(num + "e+" + places) + "e-" + places);
+}
+
+function changeDateFormat(date, format) {
+  const dateParts = date.split("/");
+  if (format === "YMD") {
+    return dateParts[2] + "/" + dateParts[1] + "/" + dateParts[0];
+  } else if (format === "MDY") {
+    return dateParts[1] + "/" + dateParts[0] + "/" + dateParts[2];
+  } else {
+    return date;
+  }
+}
+
 function App() {
   const [values, setValues] = useState([]);
+  const [edit, setEdit] = useState(false);
   const [title, setTitle] = useState("");
-  const [allValues, setAllValues] = useState([]);
+  const [dateFormat, setDateFormat] = useState("DMY");
+  const [decimals, setDecimals] = useState(4);
   const [display, setDisplay] = useState("bar");
   const [loading, setLoading] = useState(true);
   const [list, setList] = useState(true);
@@ -130,7 +146,7 @@ function App() {
                   scale: 1,
                   transition: {
                     ease: "easeOut",
-                    duration: 0.15,
+                    duration: 0.5,
                   },
                 }}
                 exit={{
@@ -138,13 +154,14 @@ function App() {
                   scale: 1,
                   transition: {
                     ease: "easeIn",
-                    duration: 0.15,
+                    duration: 0.5,
                   },
                 }}
               >
                 <div
-                  className={`h-5/6 absolute left-1/4 w-3/6 bg-electric-violet-500 rounded-lg m-2 p-2 text-electric-violet-100 overflow-auto`}
+                  className={`h-5/6 z-10 absolute left-1/4 w-3/6 bg-electric-violet-500 rounded-lg m-2 p-2 text-electric-violet-100 overflow-auto`}
                 >
+                  {/* Modal header and close button */}
                   <div className="flex items-center justify-between px-4">
                     <p className="text-electric-violet-100 font-extrabold text-xl">
                       Choose an element to see its values.
@@ -158,6 +175,7 @@ function App() {
                       x
                     </button>
                   </div>
+                  {/* Modal title input */}
                   <div className="px-4">
                     <p className="text-electric-violet-100 font-extrabold text-xl">
                       Choose your preferred title:
@@ -169,6 +187,7 @@ function App() {
                       type="text"
                     />
                   </div>
+                  {/* Modal language input */}
                   <div className="px-4">
                     <p className="text-electric-violet-100 font-extrabold text-xl">
                       Choose your preferred laguage:
@@ -198,6 +217,7 @@ function App() {
                       English
                     </button>
                   </div>
+                  {/* Modal data presentation */}
                   <div className="px-4">
                     <p className="text-electric-violet-100 font-extrabold text-xl">
                       Choose how you want to visualize the data:
@@ -228,6 +248,8 @@ function App() {
                       Chart
                     </button>
                   </div>
+                  {/* Modal options depending if you want to show a table or a
+                  graph */}
                   {!chart ? (
                     <div>
                       <div className="flex items-center justify-between px-4">
@@ -285,9 +307,61 @@ function App() {
                       </div>
                     </div>
                   ) : (
-                    <p></p>
+                    <div>
+                      <div className="flex items-center justify-between px-4">
+                        <p className="text-electric-violet-100 font-extrabold text-xl">
+                          Choose the number of decimals data will have:
+                        </p>
+                        <input
+                          value={decimals}
+                          onChange={(e) => setDecimals(e.target.value)}
+                          type="number"
+                          className="rounded px-4 py-2 text-electric-violet-900"
+                        />
+                      </div>
+                      <div className="flex items-center justify-between px-4 my-2">
+                        <p className="text-electric-violet-100 font-extrabold text-xl">
+                          Choose date format:
+                        </p>
+                        <button
+                          className={`${
+                            dateFormat === "DMY"
+                              ? "bg-electric-violet-900 transition"
+                              : "transition"
+                          } p-4 rounded-xl`}
+                          onClick={(e) => {
+                            setDateFormat("DMY");
+                          }}
+                        >
+                          DD/MM/YYYY
+                        </button>
+                        <button
+                          className={`${
+                            dateFormat === "YMD"
+                              ? "bg-electric-violet-900 transition"
+                              : "transition"
+                          } p-4 rounded-xl`}
+                          onClick={(e) => {
+                            setDateFormat("YMD");
+                          }}
+                        >
+                          YYYY/MM/DD
+                        </button>
+                        <button
+                          className={`${
+                            dateFormat === "MDY"
+                              ? "bg-electric-violet-900 transition"
+                              : "transition"
+                          } p-4 rounded-xl`}
+                          onClick={(e) => {
+                            setDateFormat("MDY");
+                          }}
+                        >
+                          MM/DD/YYYY
+                        </button>
+                      </div>
+                    </div>
                   )}
-
                   <ul className={``}>
                     {values && values.length > 0 ? (
                       values.map((index) => {
@@ -313,10 +387,15 @@ function App() {
                                       customTitle: title,
                                       values: res.data.bmx.series[0].datos,
                                       numbers: res.data.bmx.series[0].datos.map(
-                                        (a) => a.dato
+                                        (a) =>
+                                          round(
+                                            a.dato.replace(/\,/g, ""),
+                                            decimals
+                                          )
                                       ),
                                       labels: res.data.bmx.series[0].datos.map(
-                                        (a) => a.fecha
+                                        (a) =>
+                                          changeDateFormat(a.fecha, dateFormat)
                                       ),
                                       chart: chart,
                                       displayType: display,
@@ -326,6 +405,10 @@ function App() {
                                   ]);
                                   setLoading(false);
                                   setList(!list);
+                                  setBar(true);
+                                  setChart(false);
+                                  setDisplay("bar");
+                                  setDateFormat("DMY");
                                 })
                                 .catch((err) => console.log(err));
                             }}
@@ -375,9 +458,159 @@ function App() {
                     key={element.variable}
                     className="bg-electric-violet-50 my-4 p-4 rounded-lg"
                   >
-                    <h1 className="font-bold capitalize">
-                      {element.customTitle}
-                    </h1>
+                    <AnimatePresence>
+                      {edit && (
+                        <div>
+                          <div
+                            className="transition-opacity"
+                            aria-hidden="true"
+                          >
+                            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                          </div>
+                          <motion.div
+                            className="flex justify-center static px-4 sm:p-0"
+                            initial={{
+                              opacity: 0,
+                              scale: 1,
+                            }}
+                            animate={{
+                              opacity: 1,
+                              scale: 1,
+                              transition: {
+                                ease: "easeOut",
+                                duration: 0.5,
+                              },
+                            }}
+                            exit={{
+                              opacity: 0,
+                              scale: 1,
+                              transition: {
+                                ease: "easeIn",
+                                duration: 0.5,
+                              },
+                            }}
+                          >
+                            <div
+                              className={`h-auto flex flex-col absolute z-10 left-1/4 top-20 w-3/6 bg-electric-violet-500 rounded-lg p-6 text-electric-violet-100 overflow-auto`}
+                            >
+                              <p className="text-electric-violet-100 font-extrabold text-xl">
+                                Editando el {element.variable}
+                              </p>
+                              <div className="flex justify-between px-4 my-4">
+                                <p className="text-electric-violet-100 font-extrabold text-xl">
+                                  Edit your title:
+                                </p>
+                                <input
+                                  defaultValue={element.customTitle}
+                                  onChange={(e) => {
+                                    console.log(element.customTitle);
+                                    element.customTitle = e.target.value;
+                                  }}
+                                  className="rounded px-4 py-2 text-electric-violet-900"
+                                  type="text"
+                                />
+                              </div>
+                              <div className="flex items-center justify-between px-4 my-4">
+                                <p className="text-electric-violet-100 font-extrabold text-xl">
+                                  Edit the graph color:
+                                </p>
+                                <button
+                                  className={`w-12 h-6 static border-solid border-electric-violet-50 border-4`}
+                                  onClick={(e) => {
+                                    setOpenPicker(!openPicker);
+                                  }}
+                                  style={{
+                                    backgroundColor: `${blockPickerColor}`,
+                                  }}
+                                ></button>
+                                <div className="w-48">
+                                  <SliderPicker
+                                    color={blockPickerColor}
+                                    onChange={(color) => {
+                                      setBlockPickerColor(color.hex);
+                                      element.graphColor = blockPickerColor;
+                                    }}
+                                  />
+                                </div>
+                              </div>
+                              <div className="px-4">
+                                <p className="text-electric-violet-100 font-extrabold text-xl">
+                                  Edit how you want to visualize the data:
+                                </p>
+                                <button
+                                  className={`${
+                                    element.displayType &&
+                                    element.displayType === "bar"
+                                      ? "bg-electric-violet-900 transition"
+                                      : "transition"
+                                  } p-4 rounded-xl`}
+                                  onClick={(e) => {
+                                    setBar(true);
+                                    element.displayType = "bar";
+                                  }}
+                                >
+                                  Bar
+                                </button>
+                                <button
+                                  className={`${
+                                    element.displayType === "line"
+                                      ? "bg-electric-violet-900 transition"
+                                      : "transition"
+                                  } p-4 rounded-xl`}
+                                  onClick={(e) => {
+                                    setBar(false);
+                                    element.displayType = "line";
+                                  }}
+                                >
+                                  Line
+                                </button>
+                              </div>
+                              <button
+                                onClick={(e) => {
+                                  e.preventDefault();
+                                  setEdit(!edit);
+                                  setBar(true);
+                                }}
+                                className="rounded-full self-center text-2xl px-4 py-2 m-4 text-electric-violet-50 text-center bg-electric-violet-900"
+                              >
+                                Save changes
+                              </button>
+                            </div>
+                          </motion.div>
+                        </div>
+                      )}
+                    </AnimatePresence>
+                    <div className="flex justify-around items-center my-4">
+                      <h1 className="font-bold capitalize">
+                        {element.customTitle}
+                      </h1>
+                      <div>
+                        <button
+                          onClick={(e) => {
+                            console.log(`Edita esta carta ${element.variable}`);
+                            setEdit(!edit);
+                          }}
+                          className="bg-electric-violet-900 self-end hover:bg-electric-violet-600 hover:transition-colors transition-colors text-electric-violet-100 px-4 py-2 mx-2 rounded-full"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          onClick={(e) => {
+                            console.log(
+                              `Elimina esta carta ${element.variable}`
+                            );
+                            const newList = variable.filter(
+                              (item) => item.variable !== element.variable
+                            );
+                            console.log(newList);
+                            setVariable(newList);
+                          }}
+                          className="bg-electric-violet-900 self-end hover:bg-red-800 hover:transition-colors transition-colors text-electric-violet-100 px-4 py-2 rounded-full"
+                        >
+                          x
+                        </button>
+                      </div>
+                    </div>
                     {element.displayType === "bar" ? (
                       <Bar
                         options={{
@@ -430,10 +663,6 @@ function App() {
                       />
                     ) : element.displayType === "chart" ? (
                       <div>
-                        <h1 className="font-bold capitalize">
-                          {element.customTitle}
-                        </h1>
-                        <p className="capitalize">{element.variable}</p>
                         <div className="flex justify-center">
                           <Table
                             columns={[
@@ -451,7 +680,12 @@ function App() {
                                 ],
                               },
                             ]}
-                            data={element.values}
+                            data={element.labels.map(function (value, index) {
+                              return {
+                                fecha: value,
+                                dato: element.numbers[index],
+                              };
+                            })}
                           />
                         </div>
                       </div>
@@ -486,13 +720,10 @@ function App() {
             )}
           </div>
         </div>
-
       </div>
 
       <footer className="flex justify-center self-center p-4 text-center ">
-        <p>
-          Hecho con ❤️ por Rodrigo Montoya. Arequipa 2023.
-        </p>
+        <p>Hecho con ❤️ por Rodrigo Montoya. Arequipa 2023.</p>
       </footer>
     </div>
   );
